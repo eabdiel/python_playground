22d579d6f5d73e5702cd27da6f7a61c954857dcd
E. Abdiel Rodriguez <abdiel.rodriguez@live.com>
2020-09-12 18:38:47 -0400

    Add files via upload

---
+++ rpa_challenge.py
@@ -0,0 +1,61 @@
+
+#--- Modules
+import rpa as r
+import pandas as pd
+import time as time
+import requests #only needed if you are behind a proxy
+
+
+#--- Start of program
+r.init()
+#initialize TagUI - the first time you run rpa.init() the instruction will install TagUI on your AppData folder
+#                 - if you are behind a proxy, the install might error out
+#                 - see TagUI documentation for instructions on how to install it offline from a copy
+#                 - see https://github.com/tebelorg/RPA-Python/issues/94  for details
+#                 - after the initial installation you don't need to perform the setup
+
+
+#rpa.wait is a timeout - time.sleep pauses python
+time.sleep(5)
+
+r.url('http://rpachallenge.com')
+
+# rpa.download() is used here but you can also do a click() on the button
+r.download('http://rpachallenge.com/assets/downloadFiles/challenge.xlsx')
+
+#--- if you are behind a proxy - you can't use the rpa.download command; instead use requests.get
+#proxies = {"http":"yourproxy.com/:port"} #proxy* see your internet options to find out the url
+#fileurl = 'http://rpachallenge.com/assets/downloadFiles/challenge.xlsx'
+#req = requests.get(fileurl, proxies=proxies)  #this gets the source
+#output = open('challenge.xlsx', 'wb') #creates blank file
+#output.write(req.content) #populates the source into the file
+#output.close()  #closes and saves the file
+#---- at this point the file should be ready to be read
+
+#if neither the download or the requests.get work - try using rpa.click() to click on the css object to download
+
+
+# load and prepare all data to string with pandas
+df = pd.read_excel('challenge.xlsx')
+df['Phone Number'] = df['Phone Number'].astype(str)
+
+# timer starts after running this step
+r.click('//*[text()="Start"]')
+
+# loop through and fill in all fields
+for i in range(len(df.axes[0])):
+    r.type('//*[@ng-reflect-name="labelFirstName"]', df['First Name'][i])
+    r.type('//*[@ng-reflect-name="labelLastName"]', df['Last Name '][i])
+    r.type('//*[@ng-reflect-name="labelCompanyName"]', df['Company Name'][i])
+    r.type('//*[@ng-reflect-name="labelRole"]', df['Role in Company'][i])
+    r.type('//*[@ng-reflect-name="labelAddress"]', df['Address'][i])
+    r.type('//*[@ng-reflect-name="labelEmail"]', df['Email'][i])
+    r.type('//*[@ng-reflect-name="labelPhone"]', df['Phone Number'][i])
+    r.click('//*[@value="Submit"]')
+
+# page as identifier means the webpage
+r.snap('page', 'score.png')
+r.wait(10)
+r.close()
+
+#---End of program | https://github.com/eabdiel
